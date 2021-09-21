4ed875481b89071bc1a5f6774966b43b3d2bffcf
VladislavRedkin <91157551+VladislavRedkin@users.noreply.github.com>
2021-09-21 22:59:20 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <conio.h>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Hello!" << std::endl;
+	std::cout << "---------" << std::endl;
+	std::cout << "|       |" << std::endl;
+	std::cout << "|       |" << std::endl;
+	std::cout << "|       |" << std::endl;
+	std::cout << "---------" << std::endl;
+	std::cout << "       Фамилия : Редькин" << std::endl;
+	std::cout << "           Имя : Владислав" << std::endl;
+	std::cout << "      Отчество : Дмитриевич" << std::endl;
+	std::cout << "           Пол : мужской" << std::endl;
+	std::cout << " Дата рождения : 31.05.2003" << std::endl;
+	std::cout << "         Адрес : Город Днепр, улица Лодкина 3" << std::endl;
+	std::cout << "Номер телефона : 0658975869" << std::endl;
+	std::cout << "   Место учебы : ДНУ" << std::endl;
+	std::cout << "         Класс : 1 курс" << std::endl;
+	std::cout << "         Хобби : Программирование" << std::endl;
+	return 0;
+}
